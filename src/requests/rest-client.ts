43a76ca7f9c7f537b5e57c64b444f6f810d92d15
Heinrichs <Heinrichs@itc.rwth-aachen.de>
2022-09-12 15:23:39 +0200

    API Interfaces for the metadata hub

--- src/requests/rest-client.ts
+++ src/requests/rest-client.ts
@@ -1,9 +1,27 @@
+import axios from "axios";
+import {
+  Configuration,
+  MappingControllerApiFactory,
+  Rest4DoipControllerApiFactory,
+} from "./metadataHub";
+
 const metadataHubUrl = "http://d-sp23.devlef.campus.rwth-aachen.de:8080";
+
+const mappingApi = MappingControllerApiFactory(
+  new Configuration(),
+  metadataHubUrl,
+  axios
+);
+const rest4doipApi = Rest4DoipControllerApiFactory(
+  new Configuration(),
+  metadataHubUrl,
+  axios
+);
 
 export async function receiveMappings(): Promise<string[]> {
   try {
-    const response = await fetch(metadataHubUrl + "/api/v1/mapping");
-    let data = (await response.json()) as string[];
+    const response = await mappingApi.getMappings();
+    let data = response.data;
     data = data
       .map((entry) =>
         entry.includes("_") ? entry.substring(0, entry.indexOf("_")) : entry
@@ -15,4 +33,16 @@ export async function receiveMappings(): Promise<string[]> {
     console.error("Mapping cannot be received");
   }
   return [];
+}
+
+async function wrapRequest(
+  operationId: Parameters<typeof rest4doipApi.postDoipOperation>[0],
+  targetId: string,
+  token: string
+) {
+  const response = await rest4doipApi.postDoipOperation(operationId, {
+    targetId: targetId,
+    token: token,
+  });
+  return response.data;
 }
